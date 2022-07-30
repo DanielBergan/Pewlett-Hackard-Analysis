4c80ab68683103a57cc83d8676b73150f37aa05f
DanielBergan <daniel.bergan86@gmail.com>
2022-07-29 22:11:03 -0700

    added README.md

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -22,7 +22,28 @@ WHERE (to_date = '9999-01-01')
 ORDER BY emp_no, to_date DESC;
 
 SELECT COUNT (ut.emp_no), ut.title
-INTO title_count
+INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY title
 ORDER BY count DESC;
+
+SELECT DISTINCT ON (e.emp_no) 
+        e.emp_no,
+        e.first_name,
+        e.last_name,
+        e.birth_date,
+        de.from_date,
+        de.to_date,
+        ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+    ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti 
+    ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+     AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM unique_titles;
+SELECT * FROM current_emp;
